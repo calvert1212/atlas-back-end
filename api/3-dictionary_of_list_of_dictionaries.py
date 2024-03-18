fb7961d5712f6090ab8d9d1a7fdf30c8f5eab1b7
calvert1212 <calvert1212@gmail.com>
2024-03-17 23:23:51 -0500

    Update 3-dictionary_of_list_of_dictionaries.py: fixed whitespace at end

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -18,4 +18,3 @@ if __name__ == '__main__':
             } for task in to_dos if employee.get('id') == task.get('userId')
             ] for employee in users}
         json.dump(todo_dict, file)
-        
